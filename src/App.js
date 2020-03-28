b39409725126c0ea0f541e201eb132decea37e51
Rkebede <rutht.kebede@gmail.com>
2020-03-28 16:00:46 -0500

    add menu

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 const headerStyle = {
   color: "#91ecda"
 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 style={headerStyle}>Conscious Soul</h1>
-        <p>Food that awakens</p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      menuOpen: false
+    }
+  }
+
+  toggleMenu = () => {
+    this.setState({menuOpen: !menuOpen})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 style={headerStyle}>Conscious Soul</h1>
+          <p>Food that awakens</p>
+          <h2 onClick={this.toggleMenu}>MENU</h2>
+          {this.state.menuOpen && <p>Menu will go here</p>}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
