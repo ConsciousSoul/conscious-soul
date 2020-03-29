91d98864e85c8889c6164b6aaa350b4b63932d73
Rkebede <rutht.kebede@gmail.com>
2020-03-28 22:35:22 -0500

    install essential packages and add tests

---
+++ src/styles/homepage/Homepage.css.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Wrapper =  styled.div `
+  text-align: center;
+`;
+
+export const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+export const Title = styled.h1 `
+  color: #91ecda
+`;
+
+export const MenuButton = styled.h2`
+  cursor: pointer
+`
+
+
