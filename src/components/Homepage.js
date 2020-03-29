5861c6bee5c5dfb8a2ce867b323d1eae5614176c
Rkebede <rutht.kebede@gmail.com>
2020-03-29 10:44:15 -0500

    got animation working

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {Wrapper, Header, Title, MenuButton} from '../styles/homepage/Homepage.css.js';
+import React, { Component } from 'react';
+import { Wrapper, Header, Title, Button, Menu, TitleWrapper } from '../styles/homepage/Homepage.css.js';
 
 class Homepage extends Component {
   constructor(props) {
@@ -9,16 +9,36 @@ class Homepage extends Component {
     };
   }
 
+  renderContent = () => {
+    return this.state.menuOpen ? this.renderMenu() : this.renderTitle();
+  }
+
+  renderMenu = () => {
+    return (
+      <Menu>
+        <Button onClick={() => this.setState({ menuOpen: false })}>Return to Main Page</Button>
+        <p>Menu will go here</p>
+      </Menu>
+    );
+  }
+
+  renderTitle = () => {
+    return (
+      <TitleWrapper>
+        <Title>Conscious Soul</Title>
+        <p id="subtitle">Food that awakens</p>
+        <Button
+          onClick={() => this.setState({ menuOpen: true })}
+        >MENU</Button>
+      </TitleWrapper>
+    )
+  }
+
   render() {
     return (
       <Wrapper>
         <Header>
-          <Title>Conscious Soul</Title>
-          <p id="subtitle">Food that awakens</p>
-          <MenuButton 
-          onClick={() => this.setState({menuOpen: true})}
-          >MENU</MenuButton>
-          {this.state.menuOpen && <p>Menu will go here</p>}
+          {this.renderContent()}
         </Header>
       </Wrapper>
     );
